c7afc2495d76ee8a40a50b327b0d1365fce8c226
Luiz_F <103970058+LuizFilipeM@users.noreply.github.com>
2022-06-05 23:30:24 -0300

    Update Renderizador.c

--- Renderizador.c
+++ Renderizador.c
@@ -393,3 +393,17 @@ int menorcaminhoDinamica(int largura, pixel** matriz){
     }
     return min;
 }
+
+// funções para medir o tempo
+
+float diffUserTime(struct rusage *start, struct rusage *end)
+{
+    return (end->ru_utime.tv_sec - start->ru_utime.tv_sec) +
+           1e-6*(end->ru_utime.tv_usec - start->ru_utime.tv_usec);
+}
+
+float diffSystemTime(struct rusage *start, struct rusage *end)
+{
+    return (end->ru_stime.tv_sec - start->ru_stime.tv_sec) +
+           1e-6*(end->ru_stime.tv_usec - start->ru_stime.tv_usec);
+}
